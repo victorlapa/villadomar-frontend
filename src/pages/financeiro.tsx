f296fb639d37b905e8a8d95043e48eee560de302
Antonio Favarin Freire <42979335+antoniofavarinfreire@users.noreply.github.com>
2024-06-29 21:23:34 -0300

    Adicionado paginacao na tabela

--- src/pages/financeiro.tsx
+++ src/pages/financeiro.tsx
@@ -2,11 +2,14 @@ import { BaseChartCard, Page, Sidebar } from "@/components";
 import { Product } from "@/types/products";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useEffect, useState } from "react";
+import { totalmem } from "os";
 
 
 export default function Financeiro() {
 
-  const [products, setProducts] = useState<Product[] | null>();
+  const [products, setProducts ] = useState<Product[] | null>();
+  const [currentPage, setCurrentPage] = useState(1);
+
   const fetchProducts = async () => {
     const data = await fetch(
       "https://villadomarapi.azurewebsites.net/api/Products/GetProducts"
@@ -28,6 +31,26 @@ export default function Financeiro() {
       ignore = true;
     };
   }, []);
+  if (!products || products.length === 0) {
+    return <div>Carregando...</div>;
+  }
+  const itemsPerPage = 6;
+  
+  // Calcular o índice do primeiro e último item na página atual
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  
+  const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Calcular o número total de páginas
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+  
+  
+  // Mudar a página
+  const paginate = (pageNumber:number) => setCurrentPage(pageNumber);
+
+
+
   return (
     <Page>
       <Sidebar />
@@ -67,12 +90,8 @@ export default function Financeiro() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {products === null && (
-              <TableRow>
-                <TableCell>Carregando...</TableCell>
-              </TableRow>
-            )}
-            {products?.map((product) => (
+
+            { currentItems.map((product) => (
               <TableRow key={product.id}>
                 <TableCell>{product.name}</TableCell>
                 <TableCell>{product.description}</TableCell>
@@ -81,8 +100,16 @@ export default function Financeiro() {
           </TableBody>
         </Table>
           </div>
+          <div className="container w-[140px] flex justify-between ">
+            {Array.from({length: totalPages}, (_, index) => (
+              <button key={index} onClick={() => paginate(index + 1)}>
+                {index + 1 }
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </Page>
   );
 }
+
